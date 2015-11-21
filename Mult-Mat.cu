0abc1d373811acfcb960394d1046f63cdf75646a
Alejandro E. Rendon <alejorendon@utp.edu.co>
2015-11-20 20:07:47 -0500

    Modificación: Función multiplicación GPU y variables

--- Mult-Mat.cu
+++ Mult-Mat.cu
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 #include <cuda.h>
 //Height and Weight (Tamaño de la Matriz)
-#define HEIGHT 5
-#define WEIGHT 5
+#define N 10
 
 using namespace std;
 
 void fill(int *Matrix){
-    for(int i=0; i<HEIGHT; i++){
-        for(int j=0; j<WEIGHT; j++){
-            Matrix[(i*WEIGHT)+j] = 1;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            Matrix[(i*N)+j] = j;
         }
     }
 }
 
 void display(int *Matrix){
-    for(int i=0; i<HEIGHT; i++){
-        for(int j=0; j<WEIGHT; j++){
-            cout<<Matrix[i*WEIGHT+j]<<" ";
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cout<<Matrix[i*N+j]<<" ";
         }
         cout<<endl;
     }
@@ -27,34 +26,33 @@ void display(int *Matrix){
 //Multiplicación CPU
 void mult(int *A, int *B,int *C){
 	int aux = 0;
-	for(int i=0; i<HEIGHT; i++){
-		for(int j=0; j<WEIGHT; j++){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
 			aux = 0;
-			for(int k=0; k<HEIGHT; k++)
-			    aux += A[i*WEIGHT+k]* B[k*WEIGHT+j];
-			C[i*WEIGHT+j] = aux;
+			for(int k=0; k<N; k++)
+			    aux += A[i*N+k]* B[k*N+j];
+			C[i*N+j] = aux;
 		}
 	}
 }
 
 //Multiplicación GPU
-__global__ void multMat(int *d_X, int *d_Y,int *d_Z){
-	int i = blockIdx.y*blockDim.y+threadIdx.y;
-	int j = blockIdx.x*blockDim.x+threadIdx.x;
-	if(i < HEIGHT && j < WEIGHT){
-		int value = 0;
-		for(int k=0; k<HEIGHT; k++){
-			value += d_X[i*WEIGHT+k] * d_Y[k*WEIGHT+j];
-		}
-		d_Z[i*WEIGHT+j] = value;
+__global__ void multMat(int *a, int *b, int *c){
+	int k, value = 0;
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+	if(col < N && row < N) {
+		for(k = 0; k < N; k++) value += a[row * N +k] * b[k * N + col];
+		c[row * N +col]	 = value;	
 	}
 }
 
 int main(){
-	int *A = (int*)malloc(HEIGHT*WEIGHT*sizeof(int));
-	int *B = (int*)malloc(HEIGHT*WEIGHT*sizeof(int));
-	int *C = (int*)malloc(HEIGHT*WEIGHT*sizeof(int));
-	int *D = (int*)malloc(HEIGHT*WEIGHT*sizeof(int));
+	int *A = (int*)malloc(N*N*sizeof(int));
+	int *B = (int*)malloc(N*N*sizeof(int));
+	int *C = (int*)malloc(N*N*sizeof(int));
+	int *D = (int*)malloc(N*N*sizeof(int));
 
 	//Creacion de las matrices
 	fill(A);
